5864401de89cbc71d3329b95fb3cd78fda3dfb58
Admin <admin@Admins-MacBook-Pro.local>
2017-11-01 21:55:07 -0700

    Graphics Primitive

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+import UIKit
+struct Point {
+    var x: Double = 0.0
+    var y: Double = 0.0
+
+}
+var myPoint = Point(x: 9.2, y: 5.2)
+print(myPoint.x)
+print(myPoint.y)
+
+//struct Line {
+//    var start = Point()
+//    var end = Point()
+//    func length() -> Double{
+//        let length: Double = sqrt((start.x-end.x)*(start.x-end.x)*(start.y-end.y)*(start.y-end.y))
+//    return length
+//    }
+//}
+//var m = Point(x: 1.12, y: 5.32)
+//var e = Point(x: 4.52, y: 9.23)
+//
+//var z = (start: e, end: m)
+//
+//struct Triangle {
+//    var pt1 = Point()
+//    var pt2 = Point()
+//    var pt3 = Point()
+//}
+//
